5e2c589f1e04536e475d5eb3d0ed46e55389cc5f
MattStutely <git@stutelys.me.uk>
2014-05-11 17:25:30 +0100

    V1.0

---
+++ Generator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+namespace ManifestGenerator
+{
+    public class Generator
+    {
+        StringBuilder _appCache = new StringBuilder();
+        private List<string> _excludeList = new List<string>();
+        private string _rootPath;
+        public string GenerateAppCache(string rootPath, string fileToCreate)
+        {
+            _rootPath = rootPath + @"\";
+            //check if we have an exclude file and handle it
+            if (File.Exists(_rootPath + @"AppCacheExclude.txt"))
+            {
+                var excludeFile = File.ReadAllLines(_rootPath + @"\AppCacheExclude.txt");
+                _excludeList = new List<string>(excludeFile);    
+            }
+
+            _appCache.AppendLine("CACHE MANIFEST");
+            _appCache.AppendLine(string.Format("# {0}:{1}", DateTime.Now.ToString("yyyy-MM-dd"), Guid.NewGuid()));
+
+
+            //iterate every single file in the rootPath and add it to the string
+            CacheFiles(_rootPath, "*.*"); //handles root
+            DirectoryScan(_rootPath,"*.*"); //does recursion for everywhere else
+
+            _appCache.AppendLine("NETWORK:");
+            _appCache.AppendLine("*");
+            
+            string fileName = _rootPath + fileToCreate;
+            using (StreamWriter sw = new StreamWriter(fileName, false))
+            {
+                sw.Write(_appCache.ToString());    
+            }
+            
+            return String.Format("New manifest file {0} created in {1}",fileToCreate,rootPath);
+        }
+
+        private void DirectoryScan(string root, string fileMatch)
+        {
+            foreach (string dir in Directory.GetDirectories(root))
+            {
+                string excludeCheck = dir.Replace(_rootPath, "") + @"\*";
+                if (!_excludeList.Contains(excludeCheck))
+                {
+                    CacheFiles(dir, fileMatch);
+                    //recurse as necessary
+                    DirectoryScan(dir, fileMatch);    
+                }
+                
+            }
+
+        }
+
+        private void CacheFiles(string fileLocation, string fileMatch)
+        {
+            string[] files;
+            //match files in the path that meet our requirement at write them one by one to the db
+            //if there is only one then that will work nicely too!
+            files = Directory.GetFiles(fileLocation, fileMatch);
+            foreach (string file in files)
+            {
+                string fileName = file.Replace(_rootPath, "");
+                if (!_excludeList.Contains(fileName))
+                {
+                    _appCache.AppendLine(fileName.Replace(@"\", @"/"));    
+                }
+                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManifestGenerator
+{
+    public class Program
+    {
+        private static string _rootDirectory;
+        private static string _manifestFile;
+        private static FileSystemWatcher _fileSystemWatcher;
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.BackgroundColor=ConsoleColor.DarkGreen;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetWindowSize(114,24);
+                Console.Clear();
+
+                Console.WriteLine("Web App Manifest Generator (c) Matt Stutely 2014");
+                Console.WriteLine("========================================");
+                Console.WriteLine();
+
+                //how the exclude file works
+
+
+                int argsSet = args.GetUpperBound(0);
+                if (argsSet != 1)
+                {
+                    Console.WriteLine("You can set the arguments via the command line if you prefer - first argument is a string of the path to monitor, second is a string of the appcache file you want to generate");
+                    Console.WriteLine();
+
+                    //recapture, cos something wrong
+                    Console.Write(@"Root of directory to monitor (e.g. C:\MyFolder) ? ");
+                    _rootDirectory = Console.ReadLine();
+
+                    Console.Write("Filename for manifest file to generate in that directory (e.g. mycache.appcache) ? ");
+                    _manifestFile = Console.ReadLine();
+
+                    Console.WriteLine();
+                }
+                else
+                {
+                    _rootDirectory = args[0];
+                    _manifestFile = args[1];    
+                }
+
+                
+
+                _fileSystemWatcher = new FileSystemWatcher();
+                _fileSystemWatcher.Path = _rootDirectory;
+                _fileSystemWatcher.NotifyFilter = NotifyFilters.LastWrite| NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                _fileSystemWatcher.Filter = "*.*";
+                _fileSystemWatcher.Changed += new FileSystemEventHandler(FileChanged);
+                _fileSystemWatcher.Created += new FileSystemEventHandler(FileChanged);
+                _fileSystemWatcher.Deleted += new FileSystemEventHandler(FileChanged);
+                _fileSystemWatcher.Renamed += new RenamedEventHandler(FileRenamed);
+    
+                _fileSystemWatcher.EnableRaisingEvents = true;
+                Console.WriteLine(string.Format("Monitoring {0} for file changes, will save to {1}",_rootDirectory, _manifestFile));
+                System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine("Press any key to close");
+            Console.ReadLine();
+        }
+
+
+        private static void FileChanged(object source, FileSystemEventArgs e)
+        {
+            if (e.FullPath.ToLower().EndsWith("appcache")) return;
+            Console.WriteLine(string.Format("{0} Change to {1} detected",DateTime.Now.ToString(),e.FullPath));
+            RebuildManifest();
+        }
+
+        private static void FileRenamed(object source, RenamedEventArgs e)
+        {
+            if (e.FullPath.ToLower().EndsWith("appcache")) return;
+            Console.WriteLine(string.Format("{0} File renamed {1}",DateTime.Now.ToString(), e.FullPath));
+            RebuildManifest();
+        }
+
+        private static void RebuildManifest()
+        {
+            _fileSystemWatcher.EnableRaisingEvents = false;
+            var generator = new Generator();
+            generator.GenerateAppCache(_rootDirectory, _manifestFile);
+            //Console.WriteLine(generator.GenerateAppCache(_rootDirectory, _manifestFile));
+            _fileSystemWatcher.EnableRaisingEvents = true;
+        }
+    }
+}
